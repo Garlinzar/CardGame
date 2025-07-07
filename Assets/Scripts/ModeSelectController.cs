43c1e386814405c4cdd5da855b0e94ab99e59525
Kevin <kevinschaefer.981@web.de>
2025-07-07 22:04:39 +0200

    Script für Mode Select angefangen + Ingame Menu abgeschlankt

---
+++ Assets/Scripts/ModeSelectController.cs
@@ -0,0 +1,62 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ModeSelectController : MonoBehaviour
+{
+    [Header("Mode Select Buttons")]
+    [SerializeField] private Button campaignModeButton;
+    [SerializeField] private Button infiniteModeButton;
+    [SerializeField] private Button MainMenuButton;
+    [SerializeField] private Button shopButton;
+    [SerializeField] private GameObject upgradesPanel;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        // Make sure the upgrades panel is deactivated at start
+        if (upgradesPanel) upgradesPanel.SetActive(false);
+
+        // Add button listeners
+        if (campaignModeButton) campaignModeButton.onClick.AddListener(OnCampaignModeClicked);
+        if (infiniteModeButton) infiniteModeButton.onClick.AddListener(OnInfiniteModeClicked);
+        if (MainMenuButton) MainMenuButton.onClick.AddListener(OnMainMenuClicked);
+        if (shopButton) shopButton.onClick.AddListener(OnShopClicked);
+    }
+
+    private void OnCampaignModeClicked()
+    {
+        // Load the campaign mode scene
+        SceneManager.LoadScene("Main Scene");
+    }
+
+    private void OnInfiniteModeClicked()
+    {
+        // Infinite mode not implemented yet
+        Debug.Log("Infinite Mode not implemented yet!");
+    }
+
+    private void OnMainMenuClicked()
+    {
+        // Load the main menu scene
+        SceneManager.LoadScene("Main Menu");
+    }
+
+    private void OnShopClicked()
+    {
+        // Show the upgrades panel
+        if (upgradesPanel)
+        {
+            upgradesPanel.SetActive(true);
+        }
+    }
+
+    // Optional: Method to close the upgrades panel
+    public void CloseUpgradesPanel()
+    {
+        if (upgradesPanel)
+        {
+            upgradesPanel.SetActive(false);
+        }
+    }
+}
